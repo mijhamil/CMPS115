99c8d0704058be957634fd3bb5998b4c6ba13c14
Vikram <vmelkote@ucsc.edu>
2017-10-31 22:54:32 -0700

    added a check for @ucsc.edu in email when registering

--- angular-src/src/app/services/validate.service.ts
+++ angular-src/src/app/services/validate.service.ts
@@ -15,6 +15,12 @@ export class ValidateService {
 
   validateEmail(email){
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
+    var univExt = "@ucsc.edu";
+    if(email.indexOf(univExt)==-1){
+      return false;
+    }
+    else{
+      return re.test(email);
+    }
   }
 }
