655b8a20eb26f296e8c58ab8601af391fe38eadc
grant523 <grant523@gmail.com>
2017-11-07 15:11:43 -0800

    Removed posts component, edited navbar navigation when logged in, and improved post form

--- angular-src/src/app/components/dashboard/dashboard.component.ts
+++ angular-src/src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService} from '../../services/post.service'
+import {FlashMessagesService} from 'angular2-flash-messages';
 import {Router} from '@angular/router';
 
 @Component({
@@ -7,17 +9,37 @@ import {Router} from '@angular/router';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  location: String;
+  date: String;
+  time: String;
+  payrate: Number;
+  details: String;
 
   constructor(
+    private flashMessage:FlashMessagesService,
+    private postService:PostService,
     private router:Router
   ) { }
 
   ngOnInit() {
   }
 
-  doStuff() {
-    this.router.navigate(['posts']);
-    console.log("ping");
-  }
+  onPostSubmit() {
+    const post = {
+      location: this.location,
+      date: this.date,
+      time: this.time,
+      payrate: this.payrate,
+      details: this.details
+    }
 
+    this.postService.newPost(post).subscribe(data => {
+      if(data.success) {
+        this.flashMessage.show('Job posted!', {cssClass: 'alert-success', timeout: 3000});
+      }
+      else {
+        this.flashMessage.show('Job posting failed', {cssClass: 'alert-danger', timeout: 3000});
+      }
+    })
+  }
 }
