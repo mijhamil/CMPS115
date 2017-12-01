d6052d60164368ffa226921b0fdca622c37eb176
KevinsPC\Kevin <kevinajili@gmail.com>
2017-11-30 23:12:53 -0800

    Moved name to a section instead of in the title, and made it so that the list group for name and bio only show up if there is data in them

--- angular-src/src/app/components/profile/profile.component.ts
+++ angular-src/src/app/components/profile/profile.component.ts
@@ -31,13 +31,15 @@ export class ProfileComponent implements OnInit {
 
     this.authService.getOneProfile(this.currentUser.id).subscribe(profileData => {
       if(profileData.success){
-        //flash message for testing, remove later
-        // this.flashMessage.show('Account Found!', {cssClass: 'alert-success', timeout: 3000});
+        //IF data from getOneProfile for each of these fields exists, then replace
+        //  local variables with that data. Otherwise don't.
         if(profileData.user.name){ this.name = profileData.user.name;} // Set name
         if(profileData.user.username){ this.username = profileData.user.username;} // Set username
         if(profileData.user.email){ this.email = profileData.user.email;} // Set e-mail
         if(profileData.user.bio){ this.bio = profileData.user.bio;} // Set bio
         if(profileData.user.imgLink){ this.imgLink = profileData.user.imgLink;} // Set imgLink
+        //flash message for testing, remove later
+        // this.flashMessage.show('Account Found!', {cssClass: 'alert-success', timeout: 3000});
       } else{
         this.flashMessage.show('Profile data failed', {cssClass: 'alert-danger', timeout: 3000});
         // using Username field to test
