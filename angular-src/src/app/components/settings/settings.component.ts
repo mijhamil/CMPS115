26eb0a43907d3b5ff1c5a23fe6deadcb8b3d18f2
Vikram <vikrammel@live.com>
2017-11-30 23:53:47 -0800

    fixed bug in settings where db was updated with empty strings in no input was entered. Finished front end work for password reentering. Still need to implement back end functionality to hash pw when editing user profile

--- angular-src/src/app/components/settings/settings.component.ts
+++ angular-src/src/app/components/settings/settings.component.ts
@@ -14,20 +14,32 @@ import { userInfo } from 'os';
 })
 export class SettingsComponent implements OnInit {
   user = JSON.parse(localStorage.getItem('user'));
-  name = "First Last";
-  password = "password";
-  username = this.user.username;
-  //retypedPswd: String;  
-  bio = "Who even, like, are you?";
-  //skills: Array<String>;
-  imgLink = "https://i.imgur.com/6eOlEUB.jpg";
-  changes = false;
-  //prevPage = this.location.back();
 
-  //form vars
+  //form placeholder values in case of no db entries
+  _plName = "First Last";
+  _plPswd = "password";
+  _plRePswd = "Retype Password";  
+  _plBio = "Who even, like, are you?";
+  //_plSkills = ["phone repair", "knitting", "cooking"];
+  _plImgLink = "https://i.imgur.com/6eOlEUB.jpg";
+
+  //final vars to display as placeholders in forms
+  name:string;
+  oldPwHash:string;
+  email:string;
+  username:string;
+  bio:string;
+  imgLink:string;
+
+  changes = false; // track if there are changes to the form (not implemented)
+  hideRePw = true; // if true, second pw field is hidden
+
+  //vars to store form fields
   _formName:string;
   _formBio:string;
   _formImgLink:string;
+  _formPswd:string;
+  _formRetypedPwsd:string;
 
   constructor(
     private flashMessage:FlashMessagesService,
@@ -39,6 +51,16 @@ export class SettingsComponent implements OnInit {
 
   ngOnInit() {
     this.getSettings();
+  }
+
+  //change the value of hideRePw to show/hide second pw field
+  setHideRePw(){
+    if(this._formPswd && !(this._formPswd=="")){
+      this.hideRePw = false;      
+    }
+    else{
+      this.hideRePw = true;
+    }
   }
 
   //go back to previous page on cancel button press
@@ -57,10 +79,14 @@ export class SettingsComponent implements OnInit {
   getSettings() {
     this.authService.getOneProfile(this.user.id).subscribe(profile => {
       if(profile.success){
-        if(profile.user.name){this.name = profile.user.name;} // Set components name var
+        if(profile.user.name){this.name = profile.user.name;} // Set components name var to db entry
+        else if(!this.name){this.name = this._plName} //set to placeholder if no name in db
         this.username = profile.user.username;
-        if(profile.user.bio){this.bio = profile.user.bio;} // Set ... bio
-        if(profile.user.imgLink){this.imgLink = profile.user.imgLink;} // Set ... imgLink
+        this.oldPwHash = profile.user.password;
+        if(profile.user.bio){this.bio = profile.user.bio;} // Set component's bio var to db entry
+        else if(!this.bio){this.bio = this._plBio} //set to placeholder if no bio in db        
+        if(profile.user.imgLink){this.imgLink = profile.user.imgLink;} // Set component's imgLink var to db entry
+        else if(!this.imgLink){this.imgLink = this._plImgLink} //set to placeholder if no bio in db                
       } else{
         this.flashMessage.show('Settings data error', {cssClass: 'alert-danger', timeout: 3000});
       }
@@ -71,10 +97,13 @@ export class SettingsComponent implements OnInit {
   onSettingsSubmit(){
     var tempName = "";
     if(this._formName){tempName=this._formName}
+    else if(this.name){tempName=this.name}
     var tempBio = "";
     if(this._formBio){tempBio=this._formBio}
+    else if(this.bio){tempBio=this.bio}
     var tempImgLink = "";
     if(this._formImgLink){tempImgLink=this._formImgLink}
+    else if(this.imgLink){tempImgLink=this.imgLink}
     const tempUser = {
       name:tempName,
       //password: this.password,
